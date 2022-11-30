c1959e460a09f0787cd0ff4e61119e11ee2950e3
TruongNguyen1305 <trngcr71305@gmail.com>
2022-12-01 01:54:49 +0700

    add create task page UI

--- frontend/src/components/CreateTask/CreateCollectorTask.js
+++ frontend/src/components/CreateTask/CreateCollectorTask.js
@@ -2,51 +2,131 @@ import { Button, Paper, IconButton, Autocomplete, TextField, FormGroup, FormCont
 import styles from './style.module.css';
 import UndoOutlinedIcon from '@mui/icons-material/UndoOutlined';
 import {useNavigate} from 'react-router-dom'
-function CreateCollectorTask() {
+import {useState} from 'react'
+import { OutlinedInput, FormControl, InputLabel, Select, MenuItem, CircularProgress } from '@mui/material';
+import VehicleItem from "../VehicleItem";
+import axios from 'axios'
+// import { useSnackbar } from 'notistack';
+// import { SnackbarProvider } from 'notistack';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+function CreateCollectorTask({disposals, vehicles}) {
+    const [disposal, setDisposal] = useState()
+    const [time, setTime] = useState(new Date());
+    const [vehicle, setVehicle] = useState('')
+    const [mcps, setMcps] = useState([])
+    const [route, setRoute] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    // const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    // const toast = (message, variantType) => {
+    //     enqueueSnackbar(message, {
+    //         variant: variantType,
+    //         action: (key) => (
+    //             <Button style={{ fontSize: '12px', fontWeight: '600' }} size='small' onClick={() => closeSnackbar(key)}>
+    //                 Dismiss
+    //             </Button>
+    //         )
+    //     });
+    // };
+
+    const handleSubmit = () => {
+        setLoading(true)
+        axios.post('https://638265da281f14ffefa75b07.mockapi.io/collector_tasks', {
+            disposal_id: disposal.id,
+            time,
+            vehicle_id: vehicle,
+            route 
+        })
+        .then(res => {
+            // toast('Tạo task thành công', 'success')
+            console.log('Tạo task thành công' ,res)
+            setLoading(false)
+        })
+        .catch(err => {
+            // toast('Tạo task thất bại', 'error')
+            console.log('Tạo task thất bại')
+            setLoading(false)
+        })
+    }
+
+    const createRoute = () => {
+        const r = mcps.join('->')
+        setRoute(r)
+    }
+
     let navigate = useNavigate()
-    return (<div className={styles.formWrapper}>
-        <div style={{display: 'flex', justifyContent: 'space-between', margin: '0 15px'}}>
-            <Paper sx={{ width: '40%', borderRadius: 3, margin: '15px 0', position: 'relative', padding: '20px' }}>
+    return (
+        <div className={styles.formWrapper}>
+            <Paper sx={{ width: '80%', borderRadius: 3, margin: '15px 0', position: 'relative', padding: '20px' }}>
                 <Autocomplete
                     disablePortal
                     id="disposal-facility"
-                    options={['ok','not ok']}
+                    options={disposals.map(disposal => disposal.name)}
                     sx={{ width: '100%'}}
                     renderInput={(params) => <TextField {...params} label="Choose Disposal Facility" />}
+                    onChange={(e) => {
+                        const disposalName = e.target.outerText
+                        setDisposal(disposals.find(disposal => disposal.name === disposalName))
+                    }}
                 />
-
+            
                 <FormGroup style={{margin: '15px 0'}}>
                     <span>Select MCPs to create route</span>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="MCP1" />
-                        <FormControlLabel control={<Checkbox />} label="MCP2" />
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="MCP1" />
-                        <FormControlLabel control={<Checkbox />} label="MCP2" />
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="MCP1" />
-                        <FormControlLabel control={<Checkbox />} label="MCP2" />
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="MCP1" />
-                        <FormControlLabel control={<Checkbox />} label="MCP2" />
+                    {disposal ? (disposal.Mcps.map(mcp => {
+                        return <FormControlLabel key={mcp.id} control={<Checkbox />} label={mcp.name} 
+                        onChange={(e)=>{
+                            if(e.target.checked){
+                                setMcps([...mcps, mcp.name])
+                            }
+                            else{
+                                setMcps(mcps.filter(m => m !== mcp.name))
+                            }
+                        }}/>
+                    })) : (<span style={{fontSize: '12px'}}>
+                            Empty
+                    </span>)}
                     </div>
-                    <Button>Create route</Button>
+                    <Button onClick={createRoute}>Create route</Button>
+                    <span>{route}</span>
                 </FormGroup>
-
-                <Autocomplete
-                    disablePortal
-                    id="vehicle"
-                    options={['ok', 'not ok']}
-                    sx={{ width: '100%' }}
-                    renderInput={(params) => <TextField {...params} label="Choose Vehicle" />}
-                />
+            
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Vehicle</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={vehicle}
+                        label="Vehicle"
+                        MenuProps={MenuProps}
+                        onChange={(e)=>{setVehicle(e.target.value)}}
+                    >
+                    {vehicles.map(v => {
+                        return <MenuItem key={v.id} value={v.id}><VehicleItem data={v}/></MenuItem>
+                    })}
+                    </Select>
+                </FormControl>
+                
+                <OutlinedInput value={time} onChange={(e)=>setTime(e.target.value)} type='date' style={{width: '100%', marginTop: '30px'}}/>
+            
+                <div className={styles.formFooter}>
+                    <Button variant='contained' className={styles.createbtn} onClick={handleSubmit}>{loading ? (<CircularProgress color="success"/>) : ('Create')}</Button>
+                    <IconButton onClick={() => { navigate('/task') }}><UndoOutlinedIcon /></IconButton>
+                </div>
             </Paper>
-            <div style={{display: 'flex', flexDirection: 'column'}}>
-
-            </div>
         </div>
-        <div className={styles.formFooter}>
-            <Button variant='contained' className={styles.createbtn}>Create</Button>
-            <IconButton onClick={()=>{navigate('/task')}}><UndoOutlinedIcon /></IconButton>
-        </div>
-    </div>);
+    )
 }
 
 export default CreateCollectorTask;
--- frontend/src/pages/CreateTask/index.js
+++ frontend/src/pages/CreateTask/index.js
@@ -1,14 +1,28 @@
 import { Tab, Box } from "@mui/material";
 import { TabContext, TabPanel, TabList } from "@mui/lab";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CreateCollectorTask from "../../components/CreateTask/CreateCollectorTask";
 import CreateJanitorTask from "../../components/CreateTask/CreateJanitorTask";
+import axios from 'axios'
 
 function CreateTask() {
     const [value, setValue] = useState("1")
+    const [disposals, setDisposals] = useState([])
+    const [vehicles, setVehicles] = useState([])
     const handleChange = (e, value) => {
         setValue(value)
     }
+
+    useEffect(()=>{
+        axios.get('https://63823d929842ca8d3ca4bcfc.mockapi.io/DisposalFacility')
+            .then((response)=>{setDisposals(response.data)})
+            .catch(error => console.log(error))
+
+        axios.get('https://63823d929842ca8d3ca4bcfc.mockapi.io/vehicles')
+            .then((response) => { setVehicles(response.data) })
+            .catch(error => console.log(error))
+    }, [])
+
     return <>
         <TabContext value={value}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -17,8 +31,8 @@ function CreateTask() {
                     <Tab style={{ fontSize: '20px' }} label="Janitor Task" value="2" />
                 </TabList>
             </Box>
-            <TabPanel value="1"><CreateCollectorTask /></TabPanel>
-            <TabPanel value="2"><CreateJanitorTask /></TabPanel>
+            <TabPanel value="1"><CreateCollectorTask disposals={disposals} vehicles={vehicles.filter(vehicle => vehicle.isCollectVehicle)}/></TabPanel>
+            <TabPanel value="2"><CreateJanitorTask disposals={disposals} vehicles={vehicles.filter(vehicle => !vehicle.isCollectVehicle)} /></TabPanel>
         </TabContext>
     </>;
 }
